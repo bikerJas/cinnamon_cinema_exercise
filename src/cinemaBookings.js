74f0cf80ae715b4538ba0225904abc3367687094
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-15 19:29:04 +0000

    first passing test for no tickets requested

--- src/cinemaBookings.js
+++ src/cinemaBookings.js
@@ -2,6 +2,8 @@ const ticketAvailability = require("./seatAllocation.js");
 
 function tickets(bookings){
 
+    if (!bookings) throw new Error ("There is no request for any tickets");
+
 }
 
 module.exports = tickets;
