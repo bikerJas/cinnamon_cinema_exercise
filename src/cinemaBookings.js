be24d84f515970aa6a3e18f7fdb69464ca5be393
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-15 19:51:15 +0000

    second passing test added

--- src/cinemaBookings.js
+++ src/cinemaBookings.js
@@ -3,6 +3,7 @@ const seats = require("./seatAllocation.js");
 function tickets(bookings){
 
     if (!bookings) throw new Error ("There is no request for any tickets");
+    if (bookings > 3) throw new Error ("Requested more tickets than the total limit allowed");
 
 }
 
