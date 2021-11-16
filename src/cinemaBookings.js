9f76025f8941d22d65c23a07d7c2a4e0e50d48af
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-16 01:36:18 +0000

    third passing test added to allocate seats A1 and A2

--- src/cinemaBookings.js
+++ src/cinemaBookings.js
@@ -4,7 +4,9 @@ function tickets(bookings, seatsAvailable){
 
     if (!bookings) throw new Error ("There is no request for any tickets");
     if (bookings > 3) throw new Error ("Requested more tickets than the total limit allowed");
-    
+    let returnMessage = "";
+    returnMessage = seats(bookings, seatsAvailable);
+    return returnMessage;
 }
 
 module.exports = tickets;
--- src/seatAllocation.js
+++ src/seatAllocation.js
@@ -1,5 +1,11 @@
-function seats(requested, available){
-   
-};
+function seats(requested, available) {
+  let message = "Seat ";
+  for (let x = 0; x < requested; x++) {
+    message += available[x] + " ";
+  }
+  message += "allocated";
+
+  return message;
+}
 
 module.exports = seats;
